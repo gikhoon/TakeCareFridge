177005ccd83f74c62f31b63486efe8f71128c5ce
gikhoon <ghikhoon@naver.com>
2022-11-15 23:06:58 +0900

    add_ingredient.xml

---
+++ app/src/main/java/com/takecarefridge/AddIngredient.java
@@ -0,0 +1,14 @@
+package com.takecarefridge;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddIngredient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_ingredient);
+    }
+}
