76ad1d7e8d6798ea05ebec06c8dcb1d5ccfa9bb1
Jaehoon <jsi01234@naver.com>
2022-11-17 19:57:07 +0900

    디자인 drawable 추가

--- app/src/main/java/com/takecarefridge/AddIngredient.java
+++ app/src/main/java/com/takecarefridge/AddIngredient.java
@@ -1,8 +1,18 @@
 package com.takecarefridge;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class AddIngredient extends AppCompatActivity {
 
@@ -10,5 +20,34 @@ public class AddIngredient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_ingredient);
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        StorageReference storageRef = storage.getReference();
+
+        //String processedfood = storageRef.getPath();
+
+        ImageView iv1 = findViewById(R.id.imageview1);
+
+        StorageReference pathReference = storageRef.child("재료");
+        if (pathReference == null) {
+            Toast.makeText(AddIngredient.this, "저장소에 사진이 없습니다.", Toast.LENGTH_SHORT).show();
+        } else {
+            StorageReference submitPng = storageRef.child("재료/육류.png");
+            submitPng.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Glide.with(AddIngredient.this).load(uri).into(iv1);
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            });
+        }
+
     }
 }
+
--- app/src/main/java/com/takecarefridge/FreezerMain.java
+++ app/src/main/java/com/takecarefridge/FreezerMain.java
@@ -3,18 +3,29 @@ package com.takecarefridge;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FreezerMain extends AppCompatActivity {
+public class FreezerMain extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.freezer_main);
+
+        Button FreezerPlusBtn = findViewById(R.id.freezer_plus);
+        FreezerPlusBtn.setOnClickListener(this);
     }
 
     public void goMainActivity(View v){
         startActivity(new Intent(this, MainActivity.class));
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(FreezerMain.this, AddIngredient.class);
+        intent.putExtra("preActivity", "Freezer");
+        startActivity(intent);
     }
 }
