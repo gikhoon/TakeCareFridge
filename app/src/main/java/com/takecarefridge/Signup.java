148450d83bc8c20550948a83525760a616d6065a
David <isdnhk@naver.com>
2022-11-18 17:06:32 +0900

    11/18일 Singup 화면 구성

---
+++ app/src/main/java/com/takecarefridge/Signup.java
@@ -0,0 +1,14 @@
+package com.takecarefridge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
