775145bd501a0be55aca6339f561168f6e31eaf5
Jaehoon <jsi01234@naver.com>
2022-12-05 17:09:52 +0900

    찐마무리

--- app/src/main/java/com/takecarefridge/AddRecipe.java
+++ app/src/main/java/com/takecarefridge/AddRecipe.java
@@ -1,5 +1,7 @@
 package com.takecarefridge;
 
+import static java.lang.System.in;
+
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
@@ -25,19 +27,20 @@ import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.OnProgressListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import org.checkerframework.checker.units.qual.A;
+
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 public class AddRecipe extends AppCompatActivity {
     EditText editText_name;
@@ -47,9 +50,11 @@ public class AddRecipe extends AppCompatActivity {
     Button button_uploadRecipe;
     Uri imageUri;
     ImageView imageView;
+    String name;
+    String ingredient;
+    String link;
+    String storageImagePath;
 
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,192 +64,11 @@ public class AddRecipe extends AppCompatActivity {
         actionBar.hide();
 
         editText_name = findViewById(R.id.name_editText);
-        editText_ingredient = findViewById(R.id.name_editText);
-        editText_link = findViewById(R.id.name_editText);
+        editText_ingredient = findViewById(R.id.ingredient_editText);
+        editText_link = findViewById(R.id.link_editText);
         button_setImage = findViewById(R.id.setImage_button);
         button_uploadRecipe = findViewById(R.id.uploadRecipe);
-
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        StorageReference reference = FirebaseStorage.getInstance().getReference();
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference();
-
-        button_setImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent galleryIntent = new Intent();
-                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-                galleryIntent.setType("image/");
-                startActivity(galleryIntent);
-            }
-        });
-
-        ActivityResultLauncher<Intent> activityResult = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if( result.getResultCode() == RESULT_OK && result.getData() != null){
-
-                            imageUri = result.getData().getData();
-
-                            imageView.setImageURI(imageUri);
-                        }
-                    }
-                });
-
-        button_uploadRecipe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //선택한 이미지가 있다면
-                if (imageUri != null) {
-                    //uploadToFirebase(imageUri);
-                    String name = editText_name.getText().toString();
-                    String ingredient = editText_ingredient.getText().toString();
-                    String link = editText_link.getText().toString();
-                    insertData(name, ingredient, imageUri, link);
-                    goRecipeMain(view);
-                } else {
-                    Toast.makeText(AddRecipe.this, "사진을 선택해주세요.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        editText_name.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(editText_name.getWindowToken(), 0);    //hide keyboard
-                    return true;
-                }
-                return false;
-            }
-        });
-        editText_ingredient.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(editText_ingredient.getWindowToken(), 0);    //hide keyboard
-                    return true;
-                }
-                return false;
-            }
-        });
-        editText_link.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(editText_link.getWindowToken(), 0);    //hide keyboard
-                    return true;
-                }
-                return false;
-            }
-        });
-    }
-    private void insertData(String name, String ingredient, Uri imageUri, String link){
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        Model model = new Model(imageUri, link);
-
-        db.collection("레시피").add(name);
-        db.collection("레시피").document(name).set(model);
-        db.collection("레시피").document(name).collection("재료목록");
-    }
-
-    /* //사진 가져오기
-     ActivityResultLauncher<Intent> activityResult = registerForActivityResult(
-             new ActivityResultContracts.StartActivityForResult(),
-             new ActivityResultCallback<ActivityResult>() {
-                 @Override
-                 public void onActivityResult(ActivityResult result) {
-                     if( result.getResultCode() == RESULT_OK && result.getData() != null){
-                         imageUri = result.getData().getData();
-                         imageView.setImageURI(imageUri);
-                     }
-                 }
-             });*/
-    //파이어베이스 이미지 업로드
-    /*private void uploadToFirebase(Uri uri) {
-        StorageReference reference = FirebaseStorage.getInstance().getReference();
-        StorageReference fileRef = reference.child(System.currentTimeMillis() + "." + getFileExtension(uri));
-        fileRef.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        //이미지 모델에 담기
-                        Model model = new Model(uri.toString());
-                        //키로 아이디 생성
-                        String modelId = root.push().getKey();
-                        //데이터 넣기
-                        root.child(modelId).setValue(model);
-                        //프로그래스바 숨김
-                        progressBar.setVisibility(View.INVISIBLE);
-                        Toast.makeText(AddRecipe.this, "업로드 성공", Toast.LENGTH_SHORT).show();
-                        imageView.setImageResource(R.drawable.background_bar);
-                    }
-                });
-            }
-        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
-                //프로그래스바 보여주기
-                progressBar.setVisibility(View.VISIBLE);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                //프로그래스바 숨김
-                progressBar.setVisibility(View.INVISIBLE);
-                Toast.makeText(MainActivity.this, "업로드 실패", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }*/
-    //파일타입 가져오기
-    private String getFileExtension(Uri uri) {
-
-        ContentResolver cr = getContentResolver();
-        MimeTypeMap mime = MimeTypeMap.getSingleton();
-
-        return mime.getExtensionFromMimeType(cr.getType(uri));
-    }
-
-    public void goRecipeMain(View v){
-        startActivity(new Intent(this, RecipeMain.class));
-    }
-}
-/*
-public class AddRecipe extends AppCompatActivity {
-    EditText editText_name;
-    EditText editText_ingredient;
-    EditText editText_link;
-    Button button_setImage;
-    Button button_uploadRecipe;
-    Uri imageUri;
-    ImageView imageView;
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_recipe);
-
-        imageUri = null;
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-
         imageView = findViewById(R.id.ar_imageView);
-        editText_name = findViewById(R.id.name_editText);
-        editText_ingredient = findViewById(R.id.name_editText);
-        editText_link = findViewById(R.id.name_editText);
-        button_setImage = findViewById(R.id.setImage_button);
-        button_uploadRecipe = findViewById(R.id.uploadRecipe);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         StorageReference reference = FirebaseStorage.getInstance().getReference();
@@ -264,37 +88,27 @@ public class AddRecipe extends AppCompatActivity {
         button_uploadRecipe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name = editText_name.getText().toString();
-                String ingredient = editText_ingredient.getText().toString();
-                String link = editText_link.getText().toString();
-                //데이터를 입력했다면
-                if ((imageUri != null) && (name != null) && (ingredient != null) && (link != null)) {
-                    insertData(name, ingredient, imageUri, link);
+                name = editText_name.getText().toString();
+                ingredient = editText_ingredient.getText().toString();
+                link = editText_link.getText().toString();
+                //데이터를 전부 입력했다면
+                if ((imageUri != null)&&(name.length()>1)&&(ingredient.length()>1)&&(link.length()>1)) {
+                    storageImagePath = new String("레시피/"+name+".png");
+                    insertData(name, ingredient, link, storageImagePath);
+                    uploadToStorage(imageUri);
                     goRecipeMain(view);
                 } else {
-                    if (imageUri != null) {
-                        Toast.makeText(AddRecipe.this, "사진을 선택해주세요.", Toast.LENGTH_SHORT).show();
-                    }
-                    if (name.length() < 1) {
-                        Toast.makeText(AddRecipe.this, "레시피 이름을 입력해주세요.", Toast.LENGTH_SHORT).show();
-                    }
-                    if (ingredient.length() < 1) {
-                        Toast.makeText(AddRecipe.this, "레시피 재료목록을 입력해주세요.", Toast.LENGTH_SHORT).show();
-                    }
-                    if (link.length() < 1) {
-                        Toast.makeText(AddRecipe.this, "레시피 링크를 입력해주세요.", Toast.LENGTH_SHORT).show();
-                    }
+                        Toast.makeText(AddRecipe.this, "데이터를 입력해주세요.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
         editText_name.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
 
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
                     InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(editText_name.getWindowToken(), 0);    //hide keyboard
+                    imm.hideSoftInputFromWindow(editText_link.getWindowToken(), 0);    //hide keyboard
                     return true;
                 }
                 return false;
@@ -306,7 +120,7 @@ public class AddRecipe extends AppCompatActivity {
 
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
                     InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(editText_ingredient.getWindowToken(), 0);    //hide keyboard
+                    imm.hideSoftInputFromWindow(editText_link.getWindowToken(), 0);    //hide keyboard
                     return true;
                 }
                 return false;
@@ -325,31 +139,67 @@ public class AddRecipe extends AppCompatActivity {
             }
         });
     }
+    private void insertData(String name, String ingredient, String 링크, String 이미지){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //Model model = new Model(이미지, 링크);
+        HashMap<String,String> map = new HashMap<String,String>(){{//초기값 지정
+            put("링크",링크);
+            put("이미지",이미지);
+        }};
+        String ingredientList[] = ingredient.split(", ");
+
+        db.collection("레시피").document(name).set(map);
+        for (int i = 0; i < ingredientList.length; i++) {
+            재료 재료 = new 재료(ingredientList[i]);
+            db.collection("레시피").document(name).collection("재료목록").document(ingredientList[i]).set(재료);
+        }
+    }
+
+    //사진 가져오기
     ActivityResultLauncher<Intent> activityResult = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
                     if( result.getResultCode() == RESULT_OK && result.getData() != null){
-
                         imageUri = result.getData().getData();
-
-                        Log.d("11111", String.valueOf(imageUri));
-
+                        imageView = findViewById(R.id.ar_imageView);
                         imageView.setImageURI(imageUri);
+                        /*try {
+                            // 선택한 이미지에서 비트맵 생성
+                            InputStream in = getContentResolver().openInputStream(imageUri);
+                            Bitmap img = BitmapFactory.decodeStream(in);
+                            in.close();
+                            imageView.setImageBitmap(img);
+                            // 이미지뷰에 세팅
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }*/
                     }
                 }
             });
 
-    private void insertData(String name, String ingredient, Uri imageUri, String link){
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        Model model = new Model(imageUri, link);
+    //파이어베이스 이미지 업로드
+    private void uploadToStorage(Uri uri) {
+        StorageReference reference = FirebaseStorage.getInstance().getReference();
+        StorageReference fileRef = reference.child("레시피/").child(name + "." + "png");
 
-        db.collection("레시피").add(name);
-        db.collection("레시피").document(name).set(model);
-        db.collection("레시피").document(name).collection("재료목록");
+        fileRef.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Toast.makeText(AddRecipe.this, "업로드 성공", Toast.LENGTH_SHORT).show();
+
+                        imageView.setImageResource(R.drawable.background_bar);
+                    }
+                });
+
+            }
+        });
     }
-
     //파일타입 가져오기
     private String getFileExtension(Uri uri) {
 
@@ -363,4 +213,3 @@ public class AddRecipe extends AppCompatActivity {
         startActivity(new Intent(this, RecipeMain.class));
     }
 }
-*/
---
+++ app/src/main/java/com/takecarefridge/재료.java
@@ -0,0 +1,19 @@
+package com.takecarefridge;
+
+public class 재료 {
+    private String 재료;
+
+    public 재료(String 재료) {
+        this.재료 = 재료;
+    }
+
+    public String getIngredient() {
+        return 재료;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.재료 = 재료;
+    }
+
+}
+
