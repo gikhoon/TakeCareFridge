2611222f58b37552e9e271eecd36d2e41aeb2bc7
gikhoon <ghikhoon@naver.com>
2022-12-05 18:25:56 +0900

    1205 NGH 전체 게이지 (점->일)로 변경

--- app/src/main/java/com/takecarefridge/FreezerMain.java
+++ app/src/main/java/com/takecarefridge/FreezerMain.java
@@ -258,7 +258,7 @@ public class FreezerMain extends AppCompatActivity implements View.OnClickListen
                         totalEDProgress = 100;
                     }
                     pb.setProgress(totalEDProgress);
-                    freshnessTV.setText(totalEDProgress + "점");
+                    freshnessTV.setText(totalEDProgress + "일");
                 }
             }
         });
